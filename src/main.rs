326c3850367fb8f963d43dfda8f50d9302f8b36c
Robin Lange <robin.langenc@gmail.com>
2019-11-08 21:10:55 +1100

    Smaller font + colored lines

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,27 @@ use std::rc::Rc;
 use image::GrayImage;
 use unicorn_hat_hd::{UnicornHatHd, Rotate};
 
-const CHAR_SIZE: u32 = 8;
-const NB_LINES: u32 = 2;
-const FONTMAP_NB_COLS: u32 = 16;
-const FONTMAP_NB_LINES: u32 = 8;
+const CHAR_W: u32 = 4;
+const CHAR_H: u32 = 5;
+const FONTMAP_X0: u32 = 0;
+const FONTMAP_Y0: u32 = 1;
+const FONTMAP_STRIDE_X: u32 = 5;
+const FONTMAP_STRIDE_Y: u32 = 7;
+const FONTMAP_NB_COLS: u32 = 32;
+const FONTMAP_NB_LINES: u32 = 4;
+
 const SCREEN_W: u32 = 16;
 const SCREEN_H: u32 = 16;
+
+const NB_LINES: u32 = 3;
 
 fn main() {
 
     let mut board = UnicornBoard::new();
 
-    board.set_text(0, "13".into());
-    board.set_text(1, "37".into());
+    board.set_text(0, "OREW".into(), (255, 0, 0));
+    board.set_text(1, "AGUN".into(), (0, 255, 0));
+    board.set_text(2, "DAMU".into(), (0, 0, 255));
     board.display();
     
 
@@ -54,7 +62,11 @@ impl UnicornBoard {
         let mut font_map = Vec::new();
         for j in 0..FONTMAP_NB_LINES {
             for i in 0..FONTMAP_NB_COLS {
-                let char_map = dyn_image.crop(i * CHAR_SIZE, j * CHAR_SIZE, CHAR_SIZE, CHAR_SIZE).to_luma();
+                let char_map = dyn_image.crop(
+                    FONTMAP_X0 + i * FONTMAP_STRIDE_X,
+                    FONTMAP_Y0 + j * FONTMAP_STRIDE_Y,
+                    CHAR_W,
+                    CHAR_H).to_luma();
                 font_map.push(char_map);
             }
         }
@@ -62,8 +74,8 @@ impl UnicornBoard {
         font_map
     }
 
-    fn set_text(&mut self, line_index: usize, text: String) {
-        self.lines[line_index].set_text(text);
+    fn set_text(&mut self, line_index: usize, text: String, color: (u8, u8, u8)) {
+        self.lines[line_index].set_text(text, color);
     }
 
     fn display(&mut self) {
@@ -80,6 +92,7 @@ impl UnicornBoard {
 struct BoardLine {
 
     text: String,
+    color: (u8, u8, u8),
     y: u32,
     scroll: bool,
     text_offset: u32,
@@ -90,10 +103,11 @@ impl BoardLine {
 
     fn new(font_map: Rc<Vec<GrayImage>>, line_index: u32) -> BoardLine {
 
-        let y = line_index * CHAR_SIZE;
+        let y = line_index * CHAR_H;
 
         BoardLine { 
             text: "".to_owned(),
+            color: (50, 50, 50),
             y,
             scroll: false,
             text_offset: 0,
@@ -101,14 +115,15 @@ impl BoardLine {
         }
     }
 
-    fn set_text(&mut self, text: String) {
+    fn set_text(&mut self, text: String, color: (u8, u8, u8)) {
         self.text = text;
+        self.color = color;
     }
 
     fn display(&self, hat_hd: &mut UnicornHatHd) {
 
         for (i, c) in self.text.chars().enumerate() {
-            let x = (i as u32) * CHAR_SIZE;
+            let x = (i as u32) * CHAR_W;
             self.display_char(hat_hd, c, x, self.y);
         }
 
@@ -116,18 +131,14 @@ impl BoardLine {
 
     fn display_char(&self, hat_hd: &mut UnicornHatHd, c: char, x: u32, y: u32) {
 
-        let char_map = {
-            let index = c as usize;
-            if index <= 95 { &self.font_map[index - 1] }
-            else { &self.font_map[index] }
-        };
+        let char_map = &self.font_map[c as usize];
 
-        for dx in 0..CHAR_SIZE {
-            for dy in 0..CHAR_SIZE {
+        for dx in 0..CHAR_W {
+            for dy in 0..CHAR_H {
                 
                 let active = char_map.get_pixel(dx, dy)[0] > 0;
 
-                let color = if active { [50, 50, 50] } else { [0, 0, 0] };
+                let color = if active { self.color } else { (0, 0, 0) };
 
                 let (xp, yp) = ((x + dx) as usize, (y + dy) as usize);
                 hat_hd.set_pixel(xp, yp, color.into());

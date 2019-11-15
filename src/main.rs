ca64363b45a2b98e194f3359b58f44205b4fb572
Robin Lange <robin.langenc@gmail.com>
2019-11-15 21:49:11 +1100

    Take display text as arg

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 mod unicorn_board;
 
+use std::env;
 use std::thread;
 use std::time::Duration;
 use unicorn_board::{UnicornBoard, Scroll, Line};
 
 fn main() {
 
+    let text = match env::args().skip(1).next() {
+        Some(text) => text,
+        None => {
+            println!("No text specified !");
+            std::process::exit(1)
+        }
+    };
+
     let mut board = UnicornBoard::new();
 
-    let line_1 = Line::new("ILoveRust  ", 0).with_color(127, 63, 0).with_scroll(Scroll::Left(16.0));
-    let line_2 = Line::new("####", 8).with_color(127, 127, 127).with_scroll(Scroll::Left(8.0));
-
-    board.add_line(line_1);
-    board.add_line(line_2);
+    let line = Line::new(&text, 4).with_color(127, 63, 0).with_scroll(Scroll::Left(16.0));
+    board.add_line(line);
 
     loop {
         board.display();

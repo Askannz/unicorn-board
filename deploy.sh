17653f2ddadb546d036f6013d5225a9713a6a2c5
Robin Lange <robin.langenc@gmail.com>
2019-11-16 09:59:29 +1100

    deploy.sh: only build, don't run

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ IP=10.1.1.16
 
 PROJECT_NAME=$(basename $(pwd))
 rsync -vrlht --exclude=target/ --exclude=.git/ . pi@$IP:/home/pi/$PROJECT_NAME/
-ssh pi@$IP "cd $PROJECT_NAME; and cargo run"
+ssh pi@$IP "cd $PROJECT_NAME; and cargo build"
